e5eb5620f30e137635430b9a888e9fb31d248c14
20jeka08 <20evgeniy08@mail.ru>
2022-05-05 00:07:24 +0300

    main widget with title

--- codegen.py
+++ codegen.py
@@ -7,11 +7,18 @@ class CodeGen:
                         '.\\src\\ui\\components\\partswidgets\\impellerwidget\\ui_impellerwidget.py'
 
         self._command_qcc = 'pyrcc5 .\\src\\ui\\styles\\impellerwidget.qrc -o ' \
-                            '.\\src\\ui\\components\\partswidgets\\impellerwidget\\impellerwidget_rc.py'
+                            '.\\src\\ui\\impellerwidget_rc.py'
+
+        self._command_qcc_main = 'pyrcc5 .\\src\\ui\\styles\\mainwidget.qrc -o ' \
+                            '.\\src\\ui\\mainwidget_rc.py'
+
+        self._command_ui_main = 'pyuic5 -x .\\src\\ui\\mainwidget.ui -o .\\src\\ui\\ui_mainwidget.py'
 
     def generate(self):
         os.system(self._command_ui)
         os.system(self._command_qcc)
+        os.system(self._command_ui_main)
+        os.system(self._command_qcc_main)
 
 
 if __name__ == '__main__':
---
+++ src/ui/mainwidget.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QWidget
+from src.ui.ui_mainwidget import Ui_MainWidget
+from src.ui.components.partswidgets.impellerwidget.impellerwidget import ImpellerWidget
+
+
+class MainWidget(QWidget, Ui_MainWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+        self.horizontalLayout_2.addWidget(ImpellerWidget())
+
+if __name__ == '__main__':
+    from PyQt5.QtWidgets import QApplication
+    import sys
+
+    app = QApplication(sys.argv)
+    win = MainWidget()
+    win.show()
+    sys.exit(app.exec_())
+
