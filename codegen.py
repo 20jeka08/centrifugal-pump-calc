0d5da1adf6940ccd7e9cf598c313f8e415d93c9e
20jeka08 <20evgeniy08@mail.ru>
2022-05-15 14:08:10 +0300

    added stator widget to main window

--- codegen.py
+++ codegen.py
@@ -9,16 +9,27 @@ class CodeGen:
         self._command_qcc = 'pyrcc5 .\\src\\ui\\styles\\impellerwidget.qrc -o ' \
                             '.\\src\\ui\\impellerwidget_rc.py'
 
+        self._command_ui_main = 'pyuic5 -x .\\src\\ui\\mainwidget.ui -o .\\src\\ui\\ui_mainwidget.py'
+
         self._command_qcc_main = 'pyrcc5 .\\src\\ui\\styles\\mainwidget.qrc -o ' \
                             '.\\src\\ui\\mainwidget_rc.py'
 
-        self._command_ui_main = 'pyuic5 -x .\\src\\ui\\mainwidget.ui -o .\\src\\ui\\ui_mainwidget.py'
+
+        self._command_ui_stator_vanes = 'pyuic5 -x .\\src\\ui\\components\\partswidgets\\statorvaneswidget\\' \
+                                        'statorvaneswidget.ui -o .\\src\\ui\\components\\partswidgets\\' \
+                                        'statorvaneswidget\\ui_statorvaneswidget.py'
+
+        self._command_qcc_stator_vanes = 'pyrcc5 .\\src\\ui\\styles\\statorvaneswidget.qrc -o ' \
+                                         '.\\src\\ui\\statorvaneswidget_rc.py'
+
 
     def generate(self):
         os.system(self._command_ui)
         os.system(self._command_qcc)
         os.system(self._command_ui_main)
         os.system(self._command_qcc_main)
+        os.system(self._command_ui_stator_vanes)
+        os.system(self._command_qcc_stator_vanes)
 
 
 if __name__ == '__main__':
--- src/ui/components/partswidgets/statorvaneswidget/statorvaneswidget.py
+++ src/ui/components/partswidgets/statorvaneswidget/statorvaneswidget.py
@@ -1,1 +1,18 @@
 from src.api.statorvanescalc import StatorVanesCalc
+from src.ui.components.partswidgets.statorvaneswidget.ui_statorvaneswidget import Ui_StatorVanesWidget
+from PyQt5.QtWidgets import QWidget
+
+
+class StatorVanesWidget(QWidget, Ui_StatorVanesWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+if __name__ == '__main__':
+    from PyQt5.QtWidgets import QApplication
+    import sys
+
+    app = QApplication(sys.argv)
+    win = StatorVanesWidget()
+    win.show()
+    sys.exit(app.exec_())
